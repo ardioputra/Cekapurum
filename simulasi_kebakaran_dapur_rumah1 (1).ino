7c0c4d72b6022fe8eb0e53ff967fa6c296d5f8c2
Ardio Pratama Putra <64205231+doxxa11@users.noreply.github.com>
2021-03-11 23:12:17 +0700

    Add files via upload

---
+++ simulasi_kebakaran_dapur_rumah1 (1).ino
@@ -0,0 +1,65 @@
+#include<LiquidCrystal.h>  
+#include<Servo.h>
+int temp = 0;
+float gas = 0;
+int const pinServo = 2;
+int const temptmp = A0;
+int const gassen = A1;
+int led_caution = 11;
+int buzzer = 4;
+int led_fine = 3;
+int pushButton = 12;
+Servo cekservo;
+LiquidCrystal lcd(5, 6, 10, 9, 8, 7);
+void(* resetFunc)(void)=0;
+
+void setup()
+{
+  lcd.begin(16,2);
+  pinMode(temptmp, INPUT);
+  pinMode(pushButton, INPUT);
+  pinMode(led_caution, OUTPUT);
+  pinMode(led_fine, OUTPUT);
+  pinMode(gassen, INPUT);
+  pinMode(buzzer, OUTPUT);
+  cekservo.attach(pinServo);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int buttonState = digitalRead(pushButton);
+  digitalWrite(led_caution, LOW);
+  gas = analogRead(gassen);
+  temp = -40 + 0.488155 * (analogRead(temptmp) - 20);
+  if (temp >= 50 && gas >= 100) {
+    lcd.clear();
+    digitalWrite(led_caution, HIGH);
+    digitalWrite(led_fine, LOW);
+    digitalWrite(buzzer, HIGH);
+    cekservo.write(90);
+    lcd.setCursor(0,0);
+    lcd.print("Kebakaran!!");
+    lcd.setCursor(0,1);
+    lcd.print("Selamatkan Diri");
+  } else {
+    lcd.clear();
+    digitalWrite(led_fine, HIGH);
+    digitalWrite(buzzer, LOW);
+    cekservo.write(0);
+    lcd.setCursor(0,0);
+    lcd.print("Masih Aman");
+    lcd.setCursor(0,1);
+    lcd.print("Suhu: ");
+    lcd.print(temp);
+    lcd.print(" 'C");
+  }
+  if (buttonState==1){
+    cekservo.write(0);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led_fine, LOW);
+    digitalWrite(led_caution, LOW);
+    lcd.noDisplay();
+  }
+  delay(1000);
+}
