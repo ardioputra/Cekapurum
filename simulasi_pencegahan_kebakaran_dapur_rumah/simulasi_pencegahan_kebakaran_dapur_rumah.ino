5ee750989076646fa66da3d4526f5728fa5da9fc
doxxa11 <ardioputra.1109@gmail.com>
2021-03-12 17:13:20 +0700

    edit push button

--- simulasi_pencegahan_kebakaran_dapur_rumah/simulasi_pencegahan_kebakaran_dapur_rumah.ino
+++ simulasi_pencegahan_kebakaran_dapur_rumah/simulasi_pencegahan_kebakaran_dapur_rumah.ino
@@ -1,6 +1,7 @@
 #include<LiquidCrystal.h>  
 #include<Servo.h>
-int temp = 0;
+float temp = 0;
+float tmp;
 float gas = 0;
 int const pinServo = 2;
 int const temptmp = A0;
@@ -31,8 +32,11 @@ void loop()
   int buttonState = digitalRead(pushButton);
   digitalWrite(led_caution, LOW);
   gas = analogRead(gassen);
-  temp = -40 + 0.488155 * (analogRead(temptmp) - 20);
-  if (temp >= 50 && gas >= 100) {
+  tmp = analogRead(temptmp);
+  temp = tmp*5; 
+  temp = temp/1024;
+  temp = (temp-0.5)*100;
+  if (temp >= 110.5 && gas >= 100) {
     lcd.clear();
     digitalWrite(led_caution, HIGH);
     digitalWrite(led_fine, LOW);
@@ -55,12 +59,16 @@ void loop()
     lcd.print(" 'C");
   }
   if (buttonState==1){
+    lcd.clear();
     cekservo.write(0);
     digitalWrite(buzzer, LOW);
     digitalWrite(led_fine, LOW);
     digitalWrite(led_caution, LOW);
-    lcd.noDisplay();
-    resetFunc();
-  }
+    lcd.setCursor(0,0);
+    lcd.print("Sistem");
+    lcd.setCursor(0,1);
+    lcd.print("Direset!!");
+    delay(10000);
+  }  
   delay(1000);
 }
