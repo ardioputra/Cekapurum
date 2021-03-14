2d33e0d470c8307da5d56753a8d2fcfbd984f31d
doxxa11 <ardioputra.1109@gmail.com>
2021-03-15 00:40:04 +0700

    penambahan dht11

--- simulasi_pencegahan_kebakaran_dapur_rumah_DHT11/simulasi_pencegahan_kebakaran_dapur_rumah_DHT11.ino
+++ simulasi_pencegahan_kebakaran_dapur_rumah_DHT11/simulasi_pencegahan_kebakaran_dapur_rumah_DHT11.ino
@@ -1,64 +1,55 @@
 #include<LiquidCrystal.h>   //memasukan library LCD ke program        
 #include<Servo.h>           //memasukan library servo ke program
+#include<Adafruit_Sensor.h> 
+#include "DHT.h"
+#define DHTPIN 13
+#define DHTTYPE DHT11
+#define D0 16;
+#define D1 5;
+#define D2 4;
+#define D3 0;
+#define D4 2;
+#define D5 14;
+#define D6 12;
+#define D7 13;
+#define D8 15;
+#define D9 3;
+#define D10 1;
+#define Analog A0;
+DHT dht(DHTPIN, DHTTYPE)
 
-float temp = 0;
-float gas = 0;
+int led_caution = D1;                 //inisialisasi led_caution di PIN 11 pada ARDUINO UNO
+int buzzer = D3;
+int led_fine = D2;                     //inisialisasi led_fine di PIN 3 pada ARDUINO UNO
+int pushButton = D5;
+int pinServo = D4;
+int flamesen = D9;                     //ganti sesuai dengan pinout yang digunakan (digital)
 
-/*
- * untuk analog dual pinpoint
- * const int analog = A0;
- * int sensorgas = 0;
- * int sensortmp = 0;
- */
-
-const int temptmp = A0;
-const int gassen = A1;
-int led_caution = 1;                 //inisialisasi led_caution di PIN 11 pada ARDUINO UNO
-int buzzer = 4;
-int led_fine = 3;                     //inisialisasi led_fine di PIN 3 pada ARDUINO UNO
-int pushButton = 12;
-int pinServo = 2;
-int flamesen = x;                     //ganti sesuai dengan pinout yang digunakan (digital)
-
-float tmp;
+float t;
+float h;
 int flameon;
 int buttonState;
 
 Servo cekservo;                       //inisialisasi variable myservo untuk menggerakan posisi servo
-LiquidCrystal lcd(5, 6, 10, 9, 8, 7);
-
-//apabila ingin menggunakan nodemcu esp8266 rubah sesuai dengan pin yang dipasang
+LiquidCrystal lcd(D6, D7, D8, D8, D8, D8);
 
 void setup()
 {
   Serial.begin(9600);               //mendeklarasi kecepatan data rate untuk Serial Monitor
   cekservo.attach(pinServo);        //mendeklarasi pin D2 di arduino terhubung dengan kabel Signal di motor servo
   lcd.begin(16,2);
-  pinMode(flamesen, INPUT);
-  pinMode(temptmp, INPUT);
-  pinMode(pushButton, INPUT);
   pinMode(led_caution, OUTPUT);     //mendeklarasi led_caution sbg OUTPUT
   pinMode(led_fine, OUTPUT);        //mendeklarasi led_fine sbg OUTPUT
-  pinMode(gassen, INPUT);           //mendeklarasi Gas Sensor sbg INPUT
   pinMode(buzzer, OUTPUT);          //mendeklarasi Buzzer Piero sbg OUTPUT
 }
 
 void loop()
 {
   buttonState = digitalRead(pushButton);
-  flameon = digitalRead(flamesen);
-  /*
-   * nanti ganti juga variabel gas dan tmp karena kita hanya bikin satu pin analog
-   * sensorgas = analogRead(analog);
-   * sensortmp = analogRead(analog);
-   */
-  gas = analogRead(gassen);
-  tmp = analogRead(temptmp);
-  temp = tmp*5; 
-  temp = temp/1024;
-  temp = (temp-0.5)*100;
-  if (temp >= 110.5 && gas >= 100 && flameon == 1) {
-    delay(200);
+  //flameon = digitalRead(flamesen);
+  t = dht.temperature
+  h = dht.humidity
+  if (t >= 30 && h <= 30 ) {
     lcd.clear();
     digitalWrite(led_caution, HIGH);
     digitalWrite(led_fine, LOW);
@@ -69,19 +60,22 @@ void loop()
     lcd.setCursor(0,1);
     lcd.print("Selamatkan Diri");
   } else {
-    delay(200);
     lcd.clear();
     digitalWrite(led_caution, LOW);
     digitalWrite(led_fine, HIGH);       //LED nyala
     digitalWrite(buzzer, LOW);
     cekservo.write(0);
     lcd.setCursor(0,0);
-    lcd.print("Masih Aman");
+    lcd.print("Kelembapan: ");
+    lcd.print(h);
+    lcd.print("%");
     lcd.setCursor(0,1);
     lcd.print("Suhu: ");
-    lcd.print(temp);
-    lcd.print(" 'C");
+    lcd.print(t);
+    lcd.print((char)223);
+    lcd.print("C");
   }
+  
   if (buttonState==1){
     lcd.clear();
     cekservo.write(0);
