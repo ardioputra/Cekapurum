4ba78e6906bbd99188141ed374d531c3b5ff13be
doxxa11 <ardioputra.1109@gmail.com>
2021-03-15 14:34:22 +0700

    lebih simple lagi

--- cekapurum_simplified/cekapurum_simplified.ino
+++ cekapurum_simplified/cekapurum_simplified.ino
@@ -1,74 +1,64 @@
+#include<Adafruit_Sensor.h>
 #include<Servo.h>
 #include "DHT.h"
 #define DHTTYPE DHT11
-#define dht_dpin 16
+#define DHTPIN 16
 
-static const uint8_t D0   = 16;
-static const uint8_t D1   = 5;
-static const uint8_t D2   = 4;
-static const uint8_t D3   = 0;
-static const uint8_t D4   = 2;
-static const uint8_t D5   = 14;
-static const uint8_t D6   = 12;
-static const uint8_t D7   = 13;
-static const uint8_t D8   = 15;
-static const uint8_t D9   = 3;
-static const uint8_t D10  = 1;
-
-DHT dht(dht_dpin, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);
 Servo cekservo;
 
 /*
  * Ganti Pin sesuai dengan keinginan kecuali variabel f, f untuk indikator api
+ * static const uint8_t D0   = 16;
+ * static const uint8_t D1   = 5;
+ * static const uint8_t D2   = 4;
+ * static const uint8_t D3   = 0;
+ * static const uint8_t D4   = 2;
+ * static const uint8_t D5   = 14;
+ * static const uint8_t D6   = 12;
+ * static const uint8_t D7   = 13;
+ * static const uint8_t D8   = 15;
+ * static const uint8_t D9   = 3;
+ * static const uint8_t D10  = 1;
  */
 
-int led_merah = D1;
-int led_hijau = D2;
-int buzzer = D3;
-int pinServo = D4;
+int led_merah = 5;
+int led_hijau = 4;
+int buzzer = 0;
+int pinServo = 2;
 int pinFlame = A0;
 
 int f = 0;
 
 void setup() {
+  Serial.begin(115200);
   dht.begin();
   cekservo.attach(pinServo);
   cekservo.write(0);
-  Serial.begin(9600);
   Serial.println("Test Humidity and temperature\n\n");
   pinMode(led_merah, OUTPUT);
   pinMode(led_hijau, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  delay(1000);
 }
 
 void loop() {
   f = analogRead(pinFlame);
-  float h = dht.readHumidity();
-  delay(200);
   float t = dht.readTemperature();
+  Serial.print("temperature = ");
+  Serial.print(t); 
+  Serial.println("C  ");
   delay(200);
-  Serial.print(t);
-  
-  /*if(f<100 && t>=36.5){
+  if(f<100 && t>=36.5){
     digitalWrite(led_hijau, LOW);
     digitalWrite(led_merah, HIGH);
-    digitalWrite(buzzer, HIGH);
+    tone(buzzer,5000,5000);
     cekservo.write(90);
     Serial.print("Kebakaran!");
     delay(200); 
   } else {
     digitalWrite(led_hijau, HIGH);
     digitalWrite(led_merah, LOW);
-    digitalWrite(buzzer, LOW);
-    Serial.print("Current humidity = ");
-    Serial.print(h);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(t); 
-    Serial.println("C  ");
+    tone(buzzer,0,0);
     delay(1000); 
-  }*/
-  
-  delay(800); 
+  } 
 }
