212e856bd9a815a29f060f3cb575a11893748e84
doxxa11 <ardioputra.1109@gmail.com>
2021-03-17 00:04:10 +0700

    Cekapurum V1.0

--- Previous_Code/cekapurum_simplified_lcd_button/cekapurum_simplified_lcd_button.ino
+++ Previous_Code/cekapurum_simplified_lcd_button/cekapurum_simplified_lcd_button.ino
@@ -1,11 +1,14 @@
 #include<Adafruit_Sensor.h>
 #include<Servo.h>
 #include "DHT.h"
-#define   DHTTYPE DHT11
+#include <LiquidCrystal_I2C.h>
+#define DHTTYPE DHT11
 
 DHT dht(13, DHTTYPE);
 Servo cekservo;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+int button = 7; //diisi sesuai dengan pin
 int led_merah = 5;
 int led_hijau = 4;
 int buzzer = 0;
@@ -16,6 +19,7 @@ int f = 0;
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  lcd.begin();
   cekservo.attach(pinServo);
   cekservo.write(0);
   pinMode(led_merah, OUTPUT);
@@ -24,33 +28,66 @@ void setup() {
 }
 
 void loop() {
+  int buttonState = digitalRead(button);
   f = analogRead(pinFlame);
   float t = dht.readTemperature();
   float h = dht.readHumidity();
+  
   if(isnan(t)){
     Serial.println("Data Nan!");
-  }
-    Serial.println(f);
-    Serial.print("Suhu : ");
+  } else {
+    Serial.print("Tegangan Fire Sensor : ");
+    Serial.print(f*(5.0/1023.0));
+    Serial.print(" V / ");
+    Serial.print("Temp : ");
     Serial.print(t);
     Serial.print("°");
     Serial.print("C / ");
-    Serial.print("Kelembaban: ");
+    Serial.print("Hum: ");
     Serial.print(h);
     Serial.print(" %\t");
     delay(250);
+  }
   
   if(f<100 && t>40){
     digitalWrite(led_hijau, LOW);
     digitalWrite(led_merah, HIGH);
     digitalWrite(buzzer, HIGH);
     cekservo.write(90);
+    lcd.setCursor(0,0);
+    lcd.print("KEBAKARAN !!");
+    lcd.setCursor(0,1);
+    lcd.print("SELAMATKAN DIRI !!");
     delay(200); 
   } else {
     digitalWrite(led_hijau, HIGH);
     digitalWrite(led_merah, LOW);
     digitalWrite(buzzer, LOW);
     cekservo.write(0);
+    lcd.setCursor(0,0);
+    lcd.print("Suhu : ");
+    lcd.print(t);
+    lcd.print("°");
+    lcd.print("C ");
+    lcd.setCursor(0,1);
+    lcd.print("Kelembapan : ");
+    lcd.print(h);
+    lcd.print("%");
     delay(1000); 
+  }
+
+  if (buttonState==1){
+    lcd.clear();
+    cekservo.write(0);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led_hijau, LOW);
+    digitalWrite(led_merah, LOW);
+    lcd.setCursor(0,0);
+    lcd.print("Sistem");
+    lcd.setCursor(0,1);
+    lcd.print("Direset!!");
+    delay(20000);
   } 
+
+  
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Simulasi-Cekapurum
-Code Pencegahan Kebakaran Dapur Rumah menggunakan Arduino
+# Cekapurum (Pencegahan Kebakaran Dapur Rumah)
+Cekapurum menggunakan NodeMCU ESP3288 dengan sensor berupa DHT11 dan Fire Sensor
+untuk mendeteksi kebakaran di dapur rumah dan memadamkan dengan dry powder
+
+Cekapurum dilengkapi dengan indikator kebakaran berupa lampu LED sebagai tanda aman dan bahaya, 
+Buzzer mengeluarkan suara disaat ada api, 
+LCD menampilkan temperatur dan kelembapan serta saran untuk menyelamatkan diri,
+dan Servo untuk membuka katup dry chemical powder
+
+Cekapurum serta memiliki tombol reset apabila terjadi kendala
