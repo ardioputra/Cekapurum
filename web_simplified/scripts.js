21c69c3eb91641bf689325241b830c984a3b7319
doxxa11 <ardioputra.1109@gmail.com>
2021-04-14 15:12:11 +0700

    Test

--- web_simplified/scripts.js
+++ web_simplified/scripts.js
@@ -1,19 +1,16 @@
 const antares = require('antares-http');
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
 
 antares.setAccessKey('6e6b74ca7ead77f7:09437f0a4cc56904');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+app.get('/', function(req, res) {
+    antares.get('Cekapurumiot', 'CekapurumSensor')
+        .then(function(response) {
+            console.log(response.content);
+            res.json(response.content);
+        });
 });
 
-app.get('/', function(req, res, next) {
-	antares.get('Cekapurumiot', 'CekapurumSensor')
-            .then(function(response) {
-                console.log(response.content);
-                res.json(response.content);
-            });
-});
-app.listen(3000);
+app.listen(port);
